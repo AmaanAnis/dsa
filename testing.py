d40d1352ad3d400324c9ad58d9f977e1256c2a19
AmaanAnis <amaananis99@gmail.com>
2026-02-28 22:58:37 +0500

    first commit

---
+++ testing.py
@@ -0,0 +1,45 @@
+# a = [
+#     "amaan",
+#     "bashar",
+#     "sami",
+#     "mohammad",
+#     "ahmed",
+#     "ali",
+#     "hassan",
+#     "omar",
+#     "yousef",
+#     "khaled",
+#     "abdullah",
+# ]
+
+
+# def filter_words(list_of_words):
+#     list_of_a_words = list_of_words
+#     a_words = []
+#     other_words = []
+#     for a in list_of_a_words:
+#         if a.startswith("a"):
+#             # if a[0] == "a":
+#             a_words.append(a)
+#         else:
+#             other_words.append(a)
+
+#     return a_words, other_words
+
+
+# print(filter_words(a))
+
+
+# n=5
+# for i in range(0, n/2):
+#     print(i)
+
+n = 5
+u_n = n // 2
+print(u_n)
+m = 10
+
+# for i in range(0, n):
+#     for j in range(0, m):
+#         for k in range(0, n):
+#             print(f"hello {i} {j} {k}")
